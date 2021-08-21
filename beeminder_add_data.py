046d04b21fa1a39fe5823b1d66eaee773be720ee
junbuntu@U36JC <gojun077@gmail.com>
2021-08-21 23:37:52 +0900

    use HTTP session and use raise_for_status() to handle HTTP errors

--- beeminder_add_data.py
+++ beeminder_add_data.py
@@ -2,14 +2,14 @@
 # beeminder_add_data.py
 # Created on: Apr 12 2021
 # Created by: gojun077@gmail.com
-# Last Updated: Apr 12 2021
+# Last Updated: Aug 21 2021
 # Contributors: gojun077@gmail.com
 #
-# A helper script to increment a beeminder goal by + 1.0 via the Beeminder
-# API when a dual N-back session in brainworkshop is completed. The
-# Beeminder username, goalname, auth_token, and comment string will be read
-# from a json file named "beeminder.json" located in the same dir as this
-# script.
+# A helper script to increment a beeminder goal by + 1.0 as well as send
+# Brain Workshop session data in the comments field to the Beeminder API
+# when a Dual N-Back session is completed. The Beeminder username,
+# goalname, auth_token, and comment string will be read from a json file
+# named "beeminder.json" located in the same dir as this script.
 
 
 import datetime
@@ -35,8 +35,11 @@ def submit(comment_moar: str):
                        "value": 1.0,
                        "comment": full_comment}
             header = {"Content-Type": "application/json"}
-            resp_post = requests.post(endpt, headers=header,
-                                      data=json.dumps(payload))
+            sess = requests.Session()
+            resp_sess = sess.post(endpt, headers=header,
+                                  data=json.dumps(payload), timeout=5)
+            resp_sess.raise_for_status()
+            """
             if resp_post.status_code != 200:
                 print(f"HTTP Status: {resp_post.status_code}")
                 print(f"Error msg: {resp_post.text}")
@@ -45,8 +48,17 @@ def submit(comment_moar: str):
                 print("Data successfully submitted to Beeminder")
                 resp_data = resp_post.json()
                 print(resp_data)
-    except (IOError, ValueError, EOFError) as e:
-        print(e)
+            """
+            print("Data successfully submitted to Beeminder")
+            resp_data = resp_sess.json()
+            print(resp_data)
+    except requests.exceptions.ConnectionError as e:
+        raise e
+    except requests.exceptions.Timeout as e:
+        print(f"cnxn TIMEOUT: {e}")
+    # 4XX, 5XX HTTP errors
+    except requests.exceptions.HTTPError as e:
+        print(f"status code {resp_sess.status_code} from {url}: {e}")
     except:
         print("error occurred while trying to send data to Beeminder...")
 
